64a84c12e403c28bd0556a2b69f43eb48f31778c
meeklife <forsonbaah@gmail.com>
2024-02-13 15:54:26 +0000

    deployment config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  'base': 'https://github.com/meeklife/project_management_app'
 })
+
+
